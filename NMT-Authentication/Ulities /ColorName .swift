b3d0f843f924b590cf60b625d83cda580bf90b9a
Minh Tam <tamnm1996@gmail.com>
2022-04-04 19:48:01 +0700

    first commit

--- NMT-Authentication/Ulities /ColorName .swift
+++ NMT-Authentication/Ulities /ColorName .swift
@@ -5,4 +5,10 @@
 //  Created by Nguyen Minh Tam on 02/04/2022.
 //
 
-import Foundation
+import UIKit
+
+struct ColorName {
+    
+    static let color_707070 = #colorLiteral(red: 0.5141947269, green: 0.5141947269, blue: 0.5141947269, alpha: 0.8470588235)
+    
+}
--- NMT-Authentication/ViewControllers/AuthenticationMethodsViewController.swift
+++ NMT-Authentication/ViewControllers/AuthenticationMethodsViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class AuthenticationMethodsViewController: UIViewController {
+class AuthenticationMethodsViewController: BaseController {
+    //MARK: Properties
 
+    //MARK: View cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setupUI() {
+        super.setupUI()
+        
+        
     }
-    */
+    
+    //MARK: Helpers
+    
+    
+
+   
 
 }
