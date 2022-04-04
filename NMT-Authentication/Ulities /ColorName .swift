79fc6fb685d0342c2ba7e3cc3aa6e98c800f04d0
Minh Tam <tamnm1996@gmail.com>
2022-04-04 19:32:02 +0700

    first commit

---
+++ NMT-Authentication/Ulities /ColorName .swift
@@ -0,0 +1,8 @@
+//
+//  ColorName .swift
+//  NMT-Authentication
+//
+//  Created by Nguyen Minh Tam on 02/04/2022.
+//
+
+import Foundation
---
+++ NMT-Authentication/ViewControllers/AuthenticationMethodsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationMethodsViewController.swift
+//  NMT-Authentication
+//
+//  Created by Nguyen Minh Tam on 04/04/2022.
+//
+
+import UIKit
+
+class AuthenticationMethodsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
